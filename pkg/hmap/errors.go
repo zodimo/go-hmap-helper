5e49d3fa0798f25b7f878b18675bf2f444aa0d85
Jaco Labuschagne <zodimo@gmail.com>
2025-07-08 12:30:27 +0200

    Refactor error handling in hmap package by removing type parameters from NewNotFoundMapError and NewInvalidTypeMapError functions. Add Unwrap method to HMap interface for direct access to underlying map, and implement FMap function for functional mapping over HMap entries.

--- pkg/hmap/errors.go
+++ pkg/hmap/errors.go
@@ -2,10 +2,10 @@ package hmap
 
 import "errors"
 
-func NewNotFoundMapError[T any](key string) error {
+func NewNotFoundMapError(key string) error {
 	return errors.New("Key does not exist in the map: " + key)
 }
 
-func NewInvalidTypeMapError[T any](expectedType string, actualType string) error {
+func NewInvalidTypeMapError(expectedType string, actualType string) error {
 	return errors.New("invalid type, expected " + expectedType + " but got " + actualType)
 }
--- pkg/hmap/map.go
+++ pkg/hmap/map.go
@@ -8,6 +8,7 @@ type HMapKey[T any] = string
 
 type HMap[T any] interface {
 	GetWithKey(key HMapKey[T]) HMapResult[T]
+	Unwrap() map[string]any
 }
 
 var _ HMap[any] = &hmap[any]{}
@@ -17,15 +18,33 @@ type hmap[T any] struct {
 }
 
 func (h *hmap[T]) GetWithKey(key HMapKey[T]) HMapResult[T] {
-	v, ok := h.m[key]
+	v, ok := h.m[string(key)]
 	if !ok {
-		return NewMapErrorResult[T](NewNotFoundMapError[T](key))
+		return NewMapErrorResult[T](NewNotFoundMapError(key))
 	}
 
 	typedV, ok := v.(T)
 	if !ok {
-		return NewMapErrorResult[T](NewInvalidTypeMapError[T](reflect.TypeOf(new(T)).String(), reflect.TypeOf(v).String()))
+		return NewMapErrorResult[T](NewInvalidTypeMapError(reflect.TypeOf(new(T)).String(), reflect.TypeOf(v).String()))
 	}
 
 	return NewMapValidResult(typedV)
+}
+
+func (h *hmap[T]) Unwrap() map[string]any {
+	return h.m
+}
+
+func FMap[IN, OUT any](h HMap[any], key string, f func(IN) HMapResult[OUT]) HMapResult[OUT] {
+
+	v, ok := h.Unwrap()[key]
+	if !ok {
+		return NewMapErrorResult[OUT](NewNotFoundMapError(key))
+	}
+	typedV, ok := v.(IN)
+	if !ok {
+		return NewMapErrorResult[OUT](NewInvalidTypeMapError(reflect.TypeOf(new(IN)).String(), reflect.TypeOf(v).String()))
+	}
+
+	return f(typedV)
 }
