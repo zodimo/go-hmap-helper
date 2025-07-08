8e395138a336681e4bf6528085405f911ccd27d0
Jaco Labuschagne <zodimo@gmail.com>
2025-07-08 12:03:34 +0200

    Refactor HMapResult struct to use unexported fields and add methods for value retrieval and error handling.

--- pkg/hmap/result.go
+++ pkg/hmap/result.go
@@ -1,23 +1,47 @@
 package hmap
 
 type HMapResult[T any] struct {
-	Value T
-	Ok    bool
-	Err   error
+	value T
+	ok    bool
+	err   error
 }
 
 func NewMapErrorResult[T any](err error) HMapResult[T] {
 	return HMapResult[T]{
-		Value: *new(T),
-		Ok:    false,
-		Err:   err,
+		value: *new(T),
+		ok:    false,
+		err:   err,
 	}
 }
 
 func NewMapValidResult[T any](value T) HMapResult[T] {
 	return HMapResult[T]{
-		Value: value,
-		Ok:    true,
-		Err:   nil,
+		value: value,
+		ok:    true,
+		err:   nil,
 	}
+}
+
+func (r HMapResult[T]) Value() (T, error) {
+	if r.err != nil {
+		return *new(T), r.err
+	}
+
+	return r.value, nil
+}
+
+func (r HMapResult[T]) ValueOr(defaultValue T) T {
+	if !r.ok {
+		return defaultValue
+	}
+
+	return r.value
+}
+
+func (r HMapResult[T]) Ok() bool {
+	return r.ok
+}
+
+func (r HMapResult[T]) Err() error {
+	return r.err
 }
